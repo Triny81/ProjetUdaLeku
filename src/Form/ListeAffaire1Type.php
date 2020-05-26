3c18d54e47274598dfad271aff5e304a8afce485
Romain Nguyen <romainnguyen64@live.fr>
2020-05-26 15:29:25 +0200

    Avancement

--- src/Form/ListeAffaire1Type.php
+++ src/Form/ListeAffaire1Type.php
@@ -3,6 +3,10 @@
 namespace App\Form;
 
 use App\Entity\ListeAffaire;
+use App\Entity\Affaire;
+
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -14,7 +18,10 @@ class ListeAffaire1Type extends AbstractType
         $builder
             ->add('nom_francais')
             ->add('nom_basque')
-            ->add('affaire')
+            ->add('affaire', EntityType::class, ['class'=>Affaire::class,
+													  'choice_label' => 'nom_francais',
+                                                      'expanded'=>false,
+                                                      'multiple'=>false, ]) 
         ;
     }
 
